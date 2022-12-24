440941c67f5490e83efca527a29ec80fbdda95c6
unknown <toleko89@hotmail.com>
2022-12-23 23:41:18 -0500

    Algoritmo1

---
+++ Algoritmo Python/Suma.py
@@ -0,0 +1,9 @@
+# Ley de suma y Exponelcial 
+def f(n):
+    for i in range(n):
+        print(i)
+
+    for i in range(n*n):
+        print(i)
+
+f(10)
---
+++ Algoritmo Python/exponelcial.py
@@ -0,0 +1,9 @@
+# Ley exponencial
+def f(n):
+    for i in range(n):
+        for j in range(n*n):
+            print(i,j)
+
+    
+
+f(10)
